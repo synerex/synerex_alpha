6a3e32d3558b15ada8badf5c9b3a9bebd9e307b3
Ubuntu <ubuntu@ip-172-31-12-1.ap-northeast-1.compute.internal>
2018-11-02 08:07:10 +0000

    open synerex-server to global network

--- server/synerex-server.go
+++ server/synerex-server.go
@@ -407,7 +407,7 @@ func main() {
 
 	monitorapi.InitMonitor(*monitor)
 
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
 
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
