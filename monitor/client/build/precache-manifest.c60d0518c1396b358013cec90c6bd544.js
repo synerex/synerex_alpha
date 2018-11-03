60c1c7111923840820e7ef73d4469e5a84cda29a
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2018-11-03 21:48:37 +0900

    update binary build

---
+++ monitor/client/build/precache-manifest.c60d0518c1396b358013cec90c6bd544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82dc3a70b4b61e54e115",
+    "url": "/static/js/main.82dc3a70.chunk.js"
+  },
+  {
+    "revision": "0070ba3683af8c93ddd7",
+    "url": "/static/js/1.0070ba36.chunk.js"
+  },
+  {
+    "revision": "82dc3a70b4b61e54e115",
+    "url": "/static/css/main.e2dd49c9.chunk.css"
+  },
+  {
+    "revision": "09a796b6bb5c9e8f5c8c5f5fa8932ff6",
+    "url": "/index.html"
+  }
+];
