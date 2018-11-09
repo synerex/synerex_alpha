09b8753dff5b0294600f4a1531351dbac8f8df3a
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2018-11-10 00:20:39 +0900

    update limit data-store

--- server/message-store.go
+++ server/message-store.go
@@ -26,6 +26,10 @@ type message struct{
 // real struct for MessageStore
 type MessageStore struct {
 	store map[uint64]message
+	limit []uint64    // for storing message history
+	limit_pt int      // for message index
+	limit_max int     // for max number of stored message
+
 	mutex sync.RWMutex
 }
 
@@ -41,16 +45,26 @@ func (mst *MessageStore) init(){
 //	fmt.Println("Initialize LocalStore")
 	mst.store = make(map[uint64]message)
 	mst.mutex = sync.RWMutex{}
+	mst.limit_max = 1000  // todo: check size.
+	mst.limit_pt = 0
+	mst.limit = make([]uint64,mst.limit_max)
 	fmt.Println("Initialize LocalStore ",mst.store)
 }
 
+
+//todo: This is not efficient store. So we need to fix it.
 func (mst *MessageStore) AddMessage(msgType string, chType int, mid uint64, src uint64, dst uint64, arg string){
 
 	mes := message{msgType,chType, mid, src, dst, arg}
 //	fmt.Printf("AddMessage %v\n",mes)
 //	fmt.Printf("ls.store %v %d \n",ls.store, mid)
 	mst.mutex.Lock()
+	if mst.limit[mst.limit_pt] != 0 { // ring buffer, delete last one.
+		delete(mst.store, mst.limit[mst.limit_pt])
+	}
 	mst.store[mid] = mes
+	mst.limit[mst.limit_pt] = mid
+	mst.limit_pt = (mst.limit_pt+1)%mst.limit_max
 	mst.mutex.Unlock()
 //	fmt.Println("OK.")
 }
