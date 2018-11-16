636a3f79e15f63e04147972cff0b08976eadd58f
YAMANO Atsushi <yamano@sra.co.jp>
2018-11-16 16:25:00 +0900

    go fmt

--- provider/multi/multi-provider.go
+++ provider/multi/multi-provider.go
@@ -29,7 +29,7 @@ var (
 	nodesrv    = flag.String("nodesrv", "127.0.0.1:9990", "Node ID Server")
 	idlist     []uint64
 	dmMap      map[uint64]*sxutil.DemandOpts
-	myPrice	   int
+	myPrice    int
 )
 
 func init() {
@@ -47,7 +47,6 @@ func supplyCallback(clt *sxutil.SMServiceClient, sp *pb.Supply) {
 		clt.SelectSupply(sp)
 	}
 }
-
 
 // callback for each Ad Demand
 func adDemandCallback(clt *sxutil.SMServiceClient, dm *pb.Demand) {
@@ -72,7 +71,6 @@ func rideDemandCallback(clt *sxutil.SMServiceClient, dm *pb.Demand) {
 
 	// we need to ask other provider for lowest ...
 
-
 	if dm.GetDemandName() == "" { // this is Select!
 		log.Println("getSelect!")
 		clt.Confirm(sxutil.IDType(dm.GetId()))
@@ -80,10 +78,9 @@ func rideDemandCallback(clt *sxutil.SMServiceClient, dm *pb.Demand) {
 	// select any ride share demand!
 	sp := &sxutil.SupplyOpts{
 		Target: dm.GetId(),
-		Name: "RideShare by Taxi",
-		JSON: `{"Price":`+strconv.Itoa(myPrice)+`,"Distance": 5200, "Arrival": 300, "Destination": 500, "Position":{"Latitude":36.6, "Longitude":135}}`,
+		Name:   "RideShare by Taxi",
+		JSON:   `{"Price":` + strconv.Itoa(myPrice) + `,"Distance": 5200, "Arrival": 300, "Destination": 500, "Position":{"Latitude":36.6, "Longitude":135}}`,
 	} // set TargetID as Demand.Id (User will check by them)
-
 
 	clt.ProposeSupply(sp)
 
@@ -103,8 +100,6 @@ func subscribeAdDemand(client *sxutil.SMServiceClient) {
 	// comes here if channel closed
 }
 
-
-
 func subscribeRideDemand(client *sxutil.SMServiceClient) {
 	// this function should be run under goroutine!
 	ctx := context.Background() // required？
@@ -113,7 +108,7 @@ func subscribeRideDemand(client *sxutil.SMServiceClient) {
 }
 
 func sendDemand(sclient *sxutil.SMServiceClient, nm string, js string) {
-	opts := &sxutil.DemandOpts{Name: nm, JSON:js}
+	opts := &sxutil.DemandOpts{Name: nm, JSON: js}
 	id := sclient.RegisterDemand(opts)
 	idlist = append(idlist, id)
 	dmMap[id] = opts
@@ -126,7 +121,7 @@ func main() {
 	sxutil.RegisterNodeName(*nodesrv, "MultiProvider", false)
 
 	go sxutil.HandleSigInt()
-	sxutil.RegisterDeferFunction(sxutil.UnRegisterNode )
+	sxutil.RegisterDeferFunction(sxutil.UnRegisterNode)
 
 	var opts []grpc.DialOption
 	wg := sync.WaitGroup{}
@@ -136,26 +131,25 @@ func main() {
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
-	sxutil.RegisterDeferFunction(func(){conn.Close()} )
+	sxutil.RegisterDeferFunction(func() { conn.Close() })
 
 	client := pb.NewSynerexClient(conn)
 	argJson := fmt.Sprintf("{Client:Multi_AD}")
 	argJson2 := fmt.Sprintf("{Client:Multi_RIDE_SHARE}")
 
-	sclient := sxutil.NewSMServiceClient(client, pb.ChannelType_AD_SERVICE,argJson) // connection for AD
+	sclient := sxutil.NewSMServiceClient(client, pb.ChannelType_AD_SERVICE, argJson) // connection for AD
 
 	ride_sclient := sxutil.NewSMServiceClient(client, pb.ChannelType_RIDE_SHARE, argJson2) // create sclient for each type
 
 	wg.Add(1)
 	go subscribeAdDemand(sclient) // from ad company
-//	go subscribeAdSupply(sclient) // from taxi or other media
+	//	go subscribeAdSupply(sclient) // from taxi or other media
 	wg.Add(1)
 	go subscribeRideDemand(ride_sclient)
 
-//	sendDemand(sclient, "Ad to any Girls!", "{Target: \"female\", AgeFrom: 30, AgeTo:49, Spec: 0}")
+	//	sendDemand(sclient, "Ad to any Girls!", "{Target: \"female\", AgeFrom: 30, AgeTo:49, Spec: 0}")
 	wg.Wait()
 
 	sxutil.CallDeferFunctions() // cleanup!
-
 
 }
