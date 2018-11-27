fd39b971531338af27c3e1146c4916876f9f47ce
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2018-11-28 01:13:27 +0900

    update mclient for showing train traces

--- provider/map/mclient/src/component/LeafletMap.js
+++ provider/map/mclient/src/component/LeafletMap.js
@@ -23,12 +23,23 @@ var largeBusIcon = L.icon({
     iconUrl: "img/bus_above_active.png", iconSize: [64, 128], iconAnchor: [32, 64]
 });
 
+var smallTrainIcon = L.icon({
+    iconUrl: "img/train_above.png", iconSize: [16, 32], iconAnchor: [8, 16]
+});
+var midTrainIcon = L.icon({
+    iconUrl: "img/train_above.png", iconSize: [32, 64], iconAnchor: [16, 32]
+});
+var largeTrainIcon = L.icon({
+    iconUrl: "img/train_above.png", iconSize: [64, 128], iconAnchor: [32, 64]
+});
+
 
 export default class LeafletMap extends Component {
     constructor(props) {
         super(props)
         this.vehicleIcon = midVehicleIcon;
         this.busIcon = midBusIcon;
+        this.trainIcon = midTrainIcon;
     }
 
 
@@ -50,7 +61,8 @@ export default class LeafletMap extends Component {
         const position = [34.8594, 137.1720];
         let ms = []
         if( this.props.taxi){
-            let vs = this.props.store.getVehicle(1);
+            let vs = this.props.store.getVehicle(0); // Car should be ..0
+
             Object.keys(vs).forEach(function (key) {
                 ms.push(
                     <RMarker
@@ -63,7 +75,7 @@ export default class LeafletMap extends Component {
             });
         }
         if( this.props.bus){
-            let vs = this.props.store.getVehicle(4);
+            let vs = this.props.store.getVehicle(3);
             Object.keys(vs).forEach(function (key) {
                 ms.push(
                     <RMarker
@@ -76,7 +88,7 @@ export default class LeafletMap extends Component {
             });
         }
         if(this.props.busTrace){
-            let vs = this.props.store.getVehicle(4);
+            let vs = this.props.store.getVehicle(3);
             Object.keys(vs).forEach(function (key) {
 
                 let vec = vs[key]
@@ -87,11 +99,24 @@ export default class LeafletMap extends Component {
                 ms.push(
                     <Polyline
                         positions={arr}
-
                     />
                 )
             });
         }
+        if( this.props.train){
+            let vs = this.props.store.getVehicle(2);
+            Object.keys(vs).forEach(function (key) {
+                ms.push(
+                    <RMarker
+                        position={[vs[key][0][0],vs[key][0][1]]}
+                        icon={midTrainIcon}
+                        rotationOrigin={(midTrainIcon.options.iconAnchor[0] + 'px ' + midTrainIcon.options.iconAnchor[1] + 'px')}
+                        rotationAngle ={[vs[key][0][2]]}
+                    />
+                )
+            });
+        }
+
 
         let markers = <div></div>;
         if(ms.length > 0){
@@ -110,6 +135,7 @@ export default class LeafletMap extends Component {
             </Map>
         )
 
+//        console.log("zoom:"+map.status.zoom);
 
         return (
             <section className="content">
