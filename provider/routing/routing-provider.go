17f7001587886b097e4a55e7927c29df8e855501
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2018-12-08 05:40:33 +0900

    finally...

--- provider/routing/routing-provider.go
+++ provider/routing/routing-provider.go
@@ -650,7 +650,7 @@ func rideshareDemandCallback(clt *sxutil.SMServiceClient, dm *api.Demand) {
 			// train + onemile
 			// train + bus + onemile
 			//  currently we do not consider walk.
-
+			expSpecial(clt,dm)
 //			go trainAndOnemile(clt, dm)
 //			go trainAndBusAndOnemile(clt, dm)
 		}
